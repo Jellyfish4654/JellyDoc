24aeb91ed5d0c11101f48ef3366be0c479ca55cf
Jeffrey <thebaconjr@gmail.com>
2023-09-16 21:48:36 -0400

    thonk

--- next.config.js
+++ next.config.js
@@ -13,5 +13,23 @@ module.exports = withNextra({
   images: {
     unoptimized: true
   },
-  basePath: '/JellyDocs'
+  basePath: '/JellyDocs',
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter()
+    const { frontMatter } = useConfig()
+    const url =
+      'https://jellyfishbbhs.com/JellyDocs' +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+ 
+    return (
+      <>
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={frontMatter.title || 'JellyDocs'} />
+        <meta
+          property="og:description"
+          content={frontMatter.description || 'JellyDocs: The Ultimate Guide to Navigating the Sea of Knowledge'}
+        />
+      </>
+    )
+  }
 });
